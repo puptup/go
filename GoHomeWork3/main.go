96c6d34ad1fdf1e23eefa2236f1feae225ab9287
Koshevar Kirill <putumoney@gmail.com>
2020-04-13 17:02:14 +0300

    Add comments

--- GoHomeWork3/main.go
+++ GoHomeWork3/main.go
@@ -6,19 +6,25 @@ type Point struct {
 	x, y int
 }
 
+// This structure describes a square:
+// start - coordinates of the upper left point of the square
+// a - side length of a square
 type Square struct {
 	start Point
 	a     uint
 }
 
+// Method which return perimeter of a square
 func (p Square) Perimeter() uint {
 	return p.a * 4
 }
 
+//  Method which return the coordinates of the lower right point of a square
 func (p Square) End() Point {
 	return Point{p.start.x + int(p.a), p.start.y - int(p.a)}
 }
 
+// Method which return area of a square
 func (p Square) Area() uint {
 	return p.a * p.a
 }
